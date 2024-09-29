019b5e33b9740197ad2c653a0c3fcfb2eee587ba
jhon marcel adelantar <adelzarajhonmarcel@gmail.com>
2024-09-29 17:16:32 +0800

    printing hahaha

--- webpack.js
+++ webpack.js
@@ -1,12 +1,1 @@
-const userAccount = {
-    username: "jhon marcel adelantar",
-    password: "uzumaki@15",
-    gender: "male"
-}
-//adding a new key value pair
-userAccount.age = 25;
-userAccount["Nationality"] = "filipino";
-console.log(userAccount)
-console.log(userAccount.username);
-//adding a new value to the object
-userAccount.email = "adelantarjhonmarcel@gmail.com";
+console.log("hahaha")
