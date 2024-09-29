271d11bc0a48c6c3f9e74b606562eec4ea87b82a
jhon marcel adelantar <adelzarajhonmarcel@gmail.com>
2024-09-29 16:50:20 +0800

    learning the basics of js

---
+++ webpack.js
@@ -0,0 +1,9 @@
+const userAccount = {
+    username: "jhon marcel adelantar",
+    password: "uzumaki@15",
+    gender: "male"
+}
+//adding a new key value pair
+userAccount.age = 25;
+userAccount["Nationality"] = "filipino";
+console.log(userAccount)
