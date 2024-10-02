bff13004b35c1a7518083d518dc14e67eb217d2b
jhon marcel adelantar <adelzarajhonmarcel@gmail.com>
2024-10-02 19:12:47 +0800

    adding a prompt packge in the p manager

--- webpack.js
+++
@@ -1,6 +0,0 @@
-//importing prompt module
-const prompt = require("prompt-sync")({sigint: true});
-
-//creating a variable that will ask the user for his/her name
-let username = prompt("Please enter your name : ")
-console.log(`hello my name is ${username} and i am a computer engineering student studying at STI lipa`)
